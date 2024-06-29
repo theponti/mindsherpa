9315cc741472d2158dfb7e6f7255ae833c8b8442
Charles Ponti <cj@ponti.io>
2024-06-28 19:52:00 -0700

    chore: add error handling for missing environment variables

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -5,6 +5,14 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl) {
+  throw new Error('Missing env.EXPO_PUBLIC_SUPABASE_URL');
+}
+
+if (!supabaseAnonKey) {
+  throw new Error('Missing env.EXPO_PUBLIC_SUPABASE_ANON_KEY');
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
