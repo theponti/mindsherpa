0f97cae6bb77ca11acf58ce00ec56ae3d2e4f3f3
Charles Ponti <cj@ponti.io>
2024-09-01 13:24:21 -0700

    feat: produce multiple builds

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,38 @@
-export default {
-  expo: {
-    name: 'mindsherpa',
+import type { ExpoConfig, ConfigContext } from 'expo/config'
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const ENV = process.env.ENV
+  let bundleIdentifier: string
+  let name: string
+
+  switch (ENV) {
+    case 'development':
+      bundleIdentifier = 'com.pontistudios.mindsherpa.dev'
+      name = 'mindsherpa-dev'
+      break
+    case 'preview':
+    case 'production':
+    default:
+      bundleIdentifier = 'com.pontistudios.mindsherpa'
+      name = 'mindsherpa'
+  }
+
+  return {
+    ...config,
+    name,
     slug: 'mindsherpa',
     version: '1.0.0',
     scheme: 'mindsherpa',
+    owner: 'pontistudios',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    assetBundlePatterns: ['**/*'],
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
     web: {
       bundler: 'metro',
       output: 'static',
@@ -39,7 +68,7 @@ export default {
         {
           url: 'https://sentry.io/',
           project: 'mindsherpa-ios',
-          organization: 'ponti-co-llc',
+          organization: 'ponti-studios',
         },
       ],
     ],
@@ -47,18 +76,9 @@ export default {
       typedRoutes: true,
       tsconfigPaths: true,
     },
-    orientation: 'portrait',
-    icon: './assets/icon.png',
-    userInterfaceStyle: 'light',
-    splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
-    },
-    assetBundlePatterns: ['**/*'],
     ios: {
+      bundleIdentifier,
       supportsTablet: true,
-      bundleIdentifier: 'com.pontistudios.mindsherpa',
       usesAppleSignIn: true,
     },
     android: {
@@ -66,12 +86,9 @@ export default {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#ffffff',
       },
-      package: 'com.pontistudios.mindsherpa',
+      package: bundleIdentifier,
     },
     extra: {
-      router: {
-        origin: false,
-      },
       eas: {
         projectId: '6c717814-8866-46bc-b11f-edeedd1b7a69',
       },
@@ -80,5 +97,5 @@ export default {
     updates: {
       url: 'https://u.expo.dev/6c717814-8866-46bc-b11f-edeedd1b7a69',
     },
-  },
-};
+  }
+}
