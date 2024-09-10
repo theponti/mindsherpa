d8cdd7f66db27f20dc647cea02d9e6bc12a957c4
Charles Ponti <cj@ponti.io>
2024-09-09 21:16:36 -0700

    fix: redirect from drawer if no session

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,7 +1,14 @@
-import { Stack } from 'expo-router'
+import { Redirect, Stack } from 'expo-router'
 import React from 'react'
+import { useAppContext } from '~/utils/app-provider'
 
 const DrawerLayout = () => {
+  const { session } = useAppContext()
+
+  if (!session) {
+    return <Redirect href="/(auth)" />
+  }
+
   return (
     <Stack initialRouteName="focus">
       <Stack.Screen name="focus" options={{ headerShown: false }} />
